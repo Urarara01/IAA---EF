e85e034310f03a8c1b5244365203be9dd8dc074a
Oh no <kdw_luis@hotmail.com>
2025-08-21 20:05:07 -0500

    cambios con el grid

--- modal_ventas/src/panelesSistema/Listar.java
+++ modal_ventas/src/panelesSistema/Listar.java
@@ -6,7 +6,11 @@ import clasesUtiles.TabbedForm;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Listar extends TabbedForm {
 
@@ -14,11 +18,7 @@ public class Listar extends TabbedForm {
 	 * Create the panel.
 	 */
 	public Listar() {
-		setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("CListar");
-		lblNewLabel.setBounds(158, 127, 46, 14);
-		add(lblNewLabel);
+		setLayout(new GridBagLayout());
 		
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -26,8 +26,19 @@ public class Listar extends TabbedForm {
 				
 			}
 		});
+		
+		JLabel lblNewLabel = new JLabel("CListar");
+		lblNewLabel.setBounds(158, 127, 46, 14);
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		add(lblNewLabel, gbc_lblNewLabel);
 		btnNewButton.setBounds(189, 182, 89, 23);
-		add(btnNewButton);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 1;
+		add(btnNewButton, gbc_btnNewButton);
 
 	}
 
